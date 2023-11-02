c4f05c02412481e5e9ecb9b9fb740ae2f3a8b37f
Jacky Chen <jchen577@ucsc.edu>
2023-11-02 01:49:30 -0700

    Creative tilt and header comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+/*
+Name: Jacky Chen
+Game: Cloud Runner
+Time Spent: 15 hours
+Creative Tilt: One thing I am proud of is getting the finite state machine for my player to work, but we learned about that in class already.
+If I had to choose something we didn't learn in class, I'd choose the way I spawned my objects on screen. I had them randomized in an interval,
+which prevented overlapping objects and balanced which objects the player would see.
+I think my general art was pretty bad, particularly the player sprite but it was very difficult to simulate running. I'm proud of the background
+that I made because it was multiple images meaning I could layer them and attempt a parallax effect.
+*/
 const tileSize = 32;
 
 let config = {
